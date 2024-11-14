f04eb56755408149f1809af37a9ad311fb0301f7
Marareskul Alexander <maaw77@gmail.com>
2024-11-14 21:18:57 +0300

    Implented msgmngr.TestFormatRestoredHostZbx.

--- msgmngr/msgmngr_test.go
+++ msgmngr/msgmngr_test.go
@@ -3,6 +3,7 @@ package msgmngr
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"testing"
 
 	"github.com/maaw77/telbotnsn/brds"
@@ -128,6 +129,79 @@ func TestFormatProblemHostZbx(t *testing.T) {
 	if num != numNewName {
 		t.Errorf(`The string "<b>new_Host name" was not found in the required number(%d), want %d`, num, numNewName)
 	}
+}
 
+// TestFormatResotoredHostZbxEmpty  calls msgmngr.formatRestoredHostZbx with an empty input data,
+// checking for an error.
+func TestFormatRestoredHostZbxEmpty(t *testing.T) {
+	var currentHosts *brds.SavedHosts
+
+	if v, err := formatRestoredHostZbx(currentHosts); err == nil {
+		t.Fatalf(`formatProblemHosts(nil)= %s, %v, want "",error`, v, err)
+	}
+
+	currentHosts = &brds.SavedHosts{}
+	if v, err := formatRestoredHostZbx(currentHosts); err == nil {
+		t.Fatalf(`formatProblemHosts(nil)= %s, %v, want "", error`, v, err)
+	}
+}
+
+//	msgmngr.TestFormatRestoredHostZbx calls msgmngr.formatProblemHostZbx with
+//
+// various arguments, checking the correctness of the output string.
+func TestFormatRestoredHostZbx(t *testing.T) {
+	currentHosts := &brds.SavedHosts{Hosts: map[string]brds.ZabbixHost{"1": {HostIdZ: "1",
+		HostZ:     "host_1",
+		NameZ:     "name_host_1",
+		ProblemZ:  []string{"problem_1_host_1", "problem_2_host_1"},
+		ItNew:     false,
+		ItChanged: false},
+		"2": {HostIdZ: "2",
+			HostZ:     "host_2",
+			NameZ:     "name_host_2",
+			ProblemZ:  []string{"problem_1_host_2", "problem_2_host_2", "problem_3_host_2"},
+			ItNew:     false,
+			ItChanged: true},
+		"3": {HostIdZ: "3",
+			HostZ:     "host_3",
+			NameZ:     "name_host_3",
+			ProblemZ:  []string{"problem_1_host_3", "problem_2_host_3"},
+			ItNew:     false,
+			ItChanged: false},
+		"4": {HostIdZ: "4",
+			HostZ:     "host_4",
+			NameZ:     "name_host_4",
+			ProblemZ:  []string{"problem_11_host_44", "problem_2_host_4"},
+			ItNew:     true,
+			ItChanged: false},
+		"5": {HostIdZ: "5",
+			HostZ:     "host_5",
+			NameZ:     "name_host_5",
+			ProblemZ:  []string{"problem_3_host_8"},
+			ItNew:     false,
+			ItChanged: true},
+	}}
+
+	outString, err := formatRestoredHostZbx(currentHosts)
+	if err != nil {
+		t.Fatalf(`formatProblemHosts(notNil)= %s, %v, want "outString", nil`, outString, err)
+	}
 	// t.Log(outString)
+
+	currentHosts.RWD.RLock()
+	defer currentHosts.RWD.RUnlock()
+	parStr := fmt.Sprintf(`of restored hosts is %d.</b>`, len(currentHosts.Hosts))
+
+	want := regexp.MustCompile(parStr)
+	num := len(want.FindAllString(outString, -1))
+	if num != 1 {
+		t.Errorf(`The string "%s" was not found`, parStr)
+	}
+
+	for i := 1; i <= len(currentHosts.Hosts); i++ {
+		want = regexp.MustCompile(`<b>Host name:</b> name_host_` + strconv.Itoa(i))
+		if len(want.FindAllString(outString, -1)) != 1 {
+			t.Errorf(`The string "<b>Host name:<b> name_host_%s" was not found`, strconv.Itoa(i))
+		}
+	}
 }
